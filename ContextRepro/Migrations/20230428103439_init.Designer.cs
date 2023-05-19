6268e7dafe47d273a611ee0a4fb572e8820fed2e
Rafał Grądziel <rafal.gradziel@fingo.pl>
2023-05-19 15:54:23 +0200

    Upgraded EFCore to 8.0.0-preview.4.23259.3

--- ContextRepro/Migrations/20230428103439_init.Designer.cs
+++ ContextRepro/Migrations/20230428103439_init.Designer.cs
@@ -20,7 +20,7 @@ namespace ContextRepro.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "8.0.0-preview.4.23259.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
